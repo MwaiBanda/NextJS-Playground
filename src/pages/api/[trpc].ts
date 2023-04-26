ca21f8cf93cd7245b5c8431a676be4a81bd893c8
Mwai Banda <bandamwai@gmail.com>
2023-04-26 10:53:41 -0500

    Fixed OpenApiHandler

--- src/pages/api/[trpc].ts
+++ src/pages/api/[trpc].ts
@@ -3,17 +3,13 @@ import { createTRPCContext } from "~/server/api/trpc";
 import { createOpenApiNextHandler } from "trpc-openapi";
 import { appRouter } from "~/server/api/root";
 
-const handler =  (req: NextApiRequest, res: NextApiResponse) => {
-    // Setup CORS
-  
-    // Handle incoming OpenAPI requests
-    return  createOpenApiNextHandler({
-      router: appRouter,
-      createContext: createTRPCContext,
+const handler = (req: NextApiRequest, res: NextApiResponse) => {
+    return createOpenApiNextHandler({
+        router: appRouter,
+        createContext: createTRPCContext,
     })(req, res);
-  };
-  
-  export default handler;
-  
+};
 
-  
+export default handler;
+
+
